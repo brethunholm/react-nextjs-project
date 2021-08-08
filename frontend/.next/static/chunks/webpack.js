20bf30bd3c290bd9c3dbe04e1bac1689d1745866
Bre Thunholm <bthunholm@gmail.com>
2021-08-08 09:24:00 -1000

    fixed style render issue

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6d9ee33ceacb3177cf8d";
+/******/ 	var hotCurrentHash = "d1f1d89f5fb5ee62dc25";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 
+import Nprogress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => Nprogress.start());
+Router.events.on('routeChangeComplete', () => Nprogress.done());
+Router.events.on('routeChangeError', () => Nprogress.done());
 
 /* eslint-disable react/prop-types */
 export default function MyApp({ Component, pageProps }) {
